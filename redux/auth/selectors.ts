600336901f85e6017bf753d9c964c6aea6fe69db
Ecommerce <rossgaluzinskiy@gmail.com>
2024-09-09 14:57:12 +0300

    add

--- redux/auth/selectors.ts
+++ redux/auth/selectors.ts
@@ -2,10 +2,6 @@ import { RootState } from "../rootReducer";
 
 export const selectIsLoading = (state: RootState) => state.auth.isLoading;
 
-export const selectIsLoging = (state: RootState) => state.auth.isLoging;
-
-export const selectUser = (state: RootState) => state.auth.user;
-
 export const selectUserContactDetails = (state: RootState) =>
   state.auth.userContactDetails;
 
@@ -13,4 +9,3 @@ export const selectMessage = (state: RootState) => state.auth.message;
 
 export const selectError = (state: RootState) => state.auth.error;
 
-export const selectToken = (state: RootState) => state.auth.token;
